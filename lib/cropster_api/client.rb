558d84793e11a79f39737807ff4b56d826f17aff
Srikanth Kunkulagunta <srikanth.kunkulagunta@gmail.com>
2015-01-16 13:07:03 +0000

    changed client to use class methods only, with a config class and a block passing in auth variables in the initializer

--- lib/cropster_api/client.rb
+++ lib/cropster_api/client.rb
@@ -1,30 +1,37 @@
 require 'httparty'
 
 module CropsterApi
-  class Client
-    include HTTParty
-    base_uri 'https://c-sar.cropster.com/api/rest/v1'
-
-    attr_reader :username, :password, :groupcode
-    
-    def initialize opts={}
-      @username = opts[:username]
-      @password = opts[:password]
-      @groupcode = opts[:groupcode]
-    end
+  class Config
+    attr_accessor :username, :password, :groupcode
 
     def basic_auth
       {:username => username, :password => password}
     end
+  end
 
-    def roasted_lots opts={}
-      auth = opts.merge({ :basic_auth => basic_auth })
-      
-      self.class.get("/lot?groupCode=#{groupcode}&processingStep=coffee.roasting", auth)
-    end
+  class Client
+    include HTTParty
+    base_uri 'https://c-sar.cropster.com/api/rest/v1'
 
-    def green_lots
-      self.class.get("lot?groupCode=#{groupcode}&locationId=62205&processingStep=coffee.green")
+    class << self
+      attr_reader :config
+
+      def configure
+        raise ArgumentError, "block not given" unless block_given?
+
+        @config = Config.new
+        yield config
+      end
+
+      def roasted_lots opts={}
+        auth = opts.merge({ :basic_auth => config.basic_auth })
+        
+        self.get("/lot?groupCode=#{config.groupcode}&processingStep=coffee.roasting", auth)
+      end
+
+      # def green_lots
+      #   self.class.get("lot?groupCode=#{config.groupcode}&locationId=62205&processingStep=coffee.green")
+      # end
     end
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,11 +1,40 @@
 require 'spec_helper'
 
+def configure(client)
+  client.configure do |config|
+    config.username = ENV['USERNAME']
+    config.password = ENV['PASSWORD']
+    config.groupcode = ENV['GROUPCODE']
+  end
+end
+
 describe CropsterApi::Client do
-  it 'should initialize with an options hash' do
-    client = CropsterApi::Client.new({ username: 'foo', password: 'bar' })
+  let(:client){ CropsterApi::Client }
+
+  describe "#configuration" do
+    context "errors" do
+      it 'if configured without a block' do
+        expect{ client.configure }.to raise_error(ArgumentError)
+      end
+    end
+
+    it 'should configure with a block' do
+      configure(client)
+      config = client.config
+
+      expect(config.username).to eq ENV['USERNAME']
+      expect(config.password).to eq ENV['PASSWORD']
+      expect(config.groupcode).to eq ENV['GROUPCODE']
+    end
+  end
+
+  describe '#roasted_lots' do
+    before { configure(client) }
     
-    expect(client.username).to eq 'foo'
-    expect(client.password).to eq 'bar'
+    it "retrieves roasted lots" do
+      response = client.roasted_lots
+      expect(response.code).to eq 200
+    end
   end
 
   # describe "basic_auth" do
@@ -15,19 +44,4 @@ describe CropsterApi::Client do
   #   context "unauthorised" do
   #   end
   # end
-
-  describe '#roasted_lots' do
-    let(:client){ 
-      CropsterApi::Client.new(
-        username: ENV['USERNAME'],
-        password: ENV['PASSWORD'],
-        groupcode: ENV['GROUPCODE']
-      )
-    }
-    
-    it "retrieves roasted lots" do
-      response = client.roasted_lots
-      expect(response.code).to eq 200
-    end
-  end
 end
