4f2e1e9b1e9c791978a7e87e515764f17dcbe010
Srikanth Kunkulagunta <srikanth.kunkulagunta@gmail.com>
2015-01-16 21:53:15 +0000

    put request_handler specs into own file

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -45,38 +45,3 @@ describe CropsterApi::Client do
   #   end
   # end
 end
-
-describe CropsterApi::RequestHandler do
-  let(:request_handler){ CropsterApi::RequestHandler.new }
-  let(:config){ double('Config') }
-
-  before do
-    allow(config).to receive(:auth).and_return({ basic_auth: { 
-        :username=>ENV['USERNAME'],
-        :password=>ENV['PASSWORD']
-      }
-    })
-
-    allow(config).to receive(:groupcode).and_return(ENV['GROUPCODE'])
-  end
-
-  it 'handles requests' do
-    response = request_handler.trigger(config)
-    expect(response.code).to eq 200
-  end
-
-  it 'formats hash params for http' do
-    params = request_handler.parameterize_for_http(
-      processingStep: 'coffee.roasting')
-
-    expect(params).to eq "&processingStep=coffee.roasting"
-  end
-
-  it 'formats multiple hash params for http' do
-    params = request_handler.parameterize_for_http(
-      processingStep: 'coffee.roasting',
-      location_id: 123)
-
-    expect(params).to eq "&processingStep=coffee.roasting&location_id=123"
-  end
-end
