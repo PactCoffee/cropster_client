9412a24f228aea6a6d1660df36e82667713ada65
Srikanth Kunkulagunta <srikanth.kunkulagunta@gmail.com>
2015-01-26 17:21:15 +0000

    refactored to use instance variables instead of passing params around

--- lib/cropster_api/request_handler.rb
+++ lib/cropster_api/request_handler.rb
@@ -7,40 +7,39 @@ module CropsterApi
 
     base_uri "https://#{BASE}#{VERSION}"
 
-    attr_reader :config
+    attr_reader :config, :type, :params
 
     def trigger config, params={}
       @config = config
+      @type = params.extract!(:type)[:type]
+      @params = params
 
-      url = generate_url(params)
-      # params = parameterize_for_http(params)
+      url = generate_url
       self.class.get(url, config.auth)
     end
 
-    def parameterize_for_http(params={})
-      return "" if params.empty?
-      params.to_a.reduce(""){ |str,val| str+="&#{val[0].to_s.camelize(:lower)}=#{val[1]}" }
+    def parameterize_for_http
+      return "" if @params.blank?
+      @params.to_a.reduce(""){ |str,val| str+="&#{val[0].to_s.camelize(:lower)}=#{val[1]}" }
     end
 
-    def generate_url(params={})
-      type = params.extract!(:type)[:type]
-      puts type.inspect
-      raise ArgumentError, "'type' missing" if type.nil?
+    def generate_url
+      raise ArgumentError, "'type' missing" if @type.nil?
 
       case type
         when 'single lot'
-          return lot_url(params)
+          return lot_url
         when 'all lots'
-          return all_lots_url(params)
+          return all_lots_url
         when 'location'
-          return location_url(params)
+          return location_url
         else
           raise ArgumentError, "'type' not recognised, please specify 'single lot', 'all lots' or 'location'"
       end
     end
 
-    def lot_url(params={})
-      lot_id = params[:lot_id]
+    def lot_url
+      lot_id = @params[:lot_id]
 
       errors = []
       errors << "'lot_id' missing" if lot_id.nil?
@@ -51,14 +50,14 @@ module CropsterApi
       "/lot/#{lot_id}/transaction"
     end
 
-    def all_lots_url(params={})
-      params = parameterize_for_http(params)
-      "/lot?groupCode=#{config.groupcode}#{params}"
+    def all_lots_url
+      @params = parameterize_for_http
+      "/lot?groupCode=#{config.groupcode}#{@params}"
     end
 
-    def location_url(params={})
-      params = parameterize_for_http(params)
-      "/location?groupCode=#{config.groupcode}#{params}"
+    def location_url
+      @params = parameterize_for_http
+      "/location?groupCode=#{config.groupcode}#{@params}"
     end
 
     def is_integer?(num)
--- spec/request_handler_spec.rb
+++ spec/request_handler_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
+def set_var(var, val)
+  request_handler.instance_variable_set(var, val)
+end
+
 describe CropsterApi::RequestHandler do
   let(:request_handler){ CropsterApi::RequestHandler.new }
   let(:config){ double('Config') }
@@ -12,67 +16,70 @@ describe CropsterApi::RequestHandler do
     })
 
     allow(config).to receive(:groupcode).and_return(ENV['GROUPCODE'])
-    request_handler.instance_variable_set(:@config, config)
+    set_var(:@config, config)
   end
 
-  it 'handles requests' do
-    response = request_handler.trigger(config, {type: 'all lots'})
+  it 'handles requests - all lots' do
+    response = request_handler.trigger(config, { type: 'all lots' })
+    expect(response.code).to eq 200
+  end
+
+  it 'handles requests - locations' do
+    response = request_handler.trigger(config, { type: 'location' })
     expect(response.code).to eq 200
   end
 
   describe 'dynamic url generation' do
     it 'formats hash params for http' do
-      params = request_handler.parameterize_for_http(
-        processingStep: 'coffee.roasting')
+      set_var(:@params, { processingStep: 'coffee.roasting' })
 
-      expect(params).to eq "&processingStep=coffee.roasting"
+      http_params = request_handler.parameterize_for_http
+      expect(http_params).to eq "&processingStep=coffee.roasting"
     end
 
     it 'formats multiple hash params for http' do
-      params = request_handler.parameterize_for_http(
-        processingStep: 'coffee.roasting',
-        location_id: 123)
+      set_var(:@params, { processingStep: 'coffee.roasting', location_id: 123 })
 
-      expect(params).to eq "&processingStep=coffee.roasting&locationId=123"
+      http_params = request_handler.parameterize_for_http
+      expect(http_params).to eq "&processingStep=coffee.roasting&locationId=123"
     end
 
     describe 'for a specific lot' do
       it 'generates the correct url' do
-        url = request_handler.generate_url(
-          type: 'single lot',
-          lot_id: 123
-        )
+        set_var(:@params, { lot_id: 123 })
+        set_var(:@type, 'single lot')
 
+        url = request_handler.generate_url
         expect(url).to eq '/lot/123/transaction'
       end
     end
 
     describe 'for all lots' do
       it 'generates the correct url' do
-        url = request_handler.generate_url(
-          type: 'all lots'
-        )
-
+        set_var(:@type, 'all lots')
+        
+        url = request_handler.generate_url
         expect(url).to eq "/lot?groupCode=#{ENV['GROUPCODE']}"
       end
 
       it 'accepts params' do
-        url = request_handler.generate_url(
-          type: 'all lots',
+        set_var(:@type, 'all lots')
+
+        set_var(:@params, {
           location_id: 123,
           processing_step: 'coffee.roasting'
-        )
+        })
 
+        url = request_handler.generate_url
         expect(url).to eq "/lot?groupCode=#{ENV['GROUPCODE']}&locationId=123&processingStep=coffee.roasting"
       end
     end
 
     describe 'for locations' do
-    it 'generates the correct url' do
-        url = request_handler.generate_url(
-          type: 'location'
-        )
+      it 'generates the correct url' do
+        set_var(:@type, 'location')
 
+        url = request_handler.generate_url
         expect(url).to eq "/location?groupCode=#{ENV['GROUPCODE']}"
       end
     end
